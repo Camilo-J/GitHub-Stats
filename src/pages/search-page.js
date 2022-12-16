485bdb08f3240649069bd243f540afa53b18f1a3
Camilo-J <josecamilo1902@outlook.com>
2022-12-15 23:17:17 -0500

    Add styles for search

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -1,9 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Input from "../components/input";
-import { getPokemon } from "../services/pokeapi-service";
-import PokemonData from "../components/pokemon-data";
 import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  font-size: 1.25rem;
+  font-weight: 700;
+  padding-top: 2rem;
+  padding-bottom: 2.5625rem;
+`;
+
+const Form = styled("form")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  font-weight: 400;
+`;
+
+const Img = styled("img")`
+  witdh: 7.5rem;
+  height: 7.5rem;
+`;
+
+const MainView = styled("div")``;
 
 function SearchPage({ favorites, onAddFavorite, onRemoveFavorite }) {
   const [query, setQuery] = useState("");
@@ -20,6 +45,11 @@ function SearchPage({ favorites, onAddFavorite, onRemoveFavorite }) {
     favorites.find((fav) => fav.pokemon_name === pokemon?.name)
   );
 
+  useEffect(() => {
+    //Get GitApi
+    console.log("");
+  }, [query]);
+
   function handleSubmit(event) {
     event.preventDefault();
     setState({ status: "pending", data: null, error: null });
@@ -34,22 +64,22 @@ function SearchPage({ favorites, onAddFavorite, onRemoveFavorite }) {
     //       data: null,
     //       error: "El pokemon no existe! Intenta de nuevo",
     //     });
-    //   });
+    //   dx});
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <Container>
+      <Form onSubmit={handleSubmit}>
         <Input
           name="query"
-          placeholder="pokemon name"
+          placeholder="username"
           value={query}
           onChange={(event) => setQuery(event.target.value)}
         />
-        <button type="submit">Search</button>
-      </form>
-      {status === "pending" && "Loading..."}
-      {status === "idle" && "Ready to search"}
+        {/* <button type="submit">Search</button> */}
+      </Form>
+      {status === "pending" && "Retrieving user..."}
+
       {/* {status === "success" && (
         <PokemonData
           pokemon={pokemon}
@@ -58,9 +88,16 @@ function SearchPage({ favorites, onAddFavorite, onRemoveFavorite }) {
           isFavorite={isFavorite}
         />
       )} */}
+      <MainView>
+        <Img
+          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+          alt="logo"
+        />
+      </MainView>
+      {status === "idle" && "No user..."}
       {status === "error" && <p style={{ color: "red" }}>{error}</p>}
       <Link to="/favorites">Go to Favorites</Link>
-    </div>
+    </Container>
   );
 }
 
