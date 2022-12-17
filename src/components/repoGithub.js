01d85c5123ecda3a7646aded14d66ee91ae3aad0
Camilo-J <josecamilo1902@outlook.com>
2022-12-16 20:03:56 -0500

    Add list of repositories

--- src/components/repoGithub.js
+++ src/components/repoGithub.js
@@ -1,23 +1,32 @@
 import styled from "@emotion/styled";
+import Icon from "./Icon";
+import { VscRepoForked } from "react-icons/vsc";
+import { AiOutlineStar } from "react-icons/ai";
+import { BsCircleFill } from "react-icons/bs";
 
-const ContainIcon = styled("div")`
+const ContainRepo = styled("div")`
+  max-width: 350px;
+  padding: 8px 12px;
+  border: 1px solid black;
+`;
+const Icons = styled("div")`
   display: flex;
-  gap: 4.2px;
+  gap: 1rem;
 `;
 
-const Repo = () => {
-  return (
-    <div>
-      <p>titulo</p>
-      <p>descripcion</p>
-      <div>
-        <ContainIcon>
-          <p>icon</p>
+// BsCircleFill;
 
-          <p>titulo</p>
-        </ContainIcon>
-      </div>
-    </div>
+const Repo = ({ fork = 0, stars = 0, language = 0, description, name }) => {
+  return (
+    <ContainRepo>
+      <p>{name}</p>
+      <p>{description}</p>
+      <Icons>
+        <Icon icon={<BsCircleFill />} text={language} />
+        <Icon icon={<AiOutlineStar />} text={stars} />
+        <Icon icon={<VscRepoForked />} text={fork} />
+      </Icons>
+    </ContainRepo>
   );
 };
 
