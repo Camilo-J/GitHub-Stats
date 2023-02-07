064e5c565652f1106ffba136c9896e4171055909
Camilo-J <josecamilo1902@outlook.com>
2023-02-07 12:04:36 -0500

    Fix bugs in profiles' photos

--- src/pages/followers-page.js
+++ src/pages/followers-page.js
@@ -130,18 +130,15 @@ function FollowersPage({ profile }) {
         <img src={Image1} alt="" onClick={handleIncresePage}></img>
       </Pagination>
 
-      {followers.map((follower, index) => (
-        <FollowersContainer>
+      <FollowersContainer>
+        {followers.map((follower, index) => (
           <FollowersCard value={follower.login} key={`unq${index}`}>
-            <PokeImage
-              src="https://s3-alpha-sig.figma.com/img/5215/dacf/3b688b2a3dd2a02199de17ea0d7b5c09?Expires=1672012800&Signature=owRR5x8O9uqQQ1lQW3Ip4weQEhZfnrgUDiB6KoSAnCvLSxdDh8G6VbQzgKsj0IL9-tH8ADoo7AaUI2vOtbgtUo137po0DDuNzeai4ymqHMxuLEvhANS60nXzdg6tF6DBV6rs6kkuao28IT4pr3PVzqH-Lxz82mMb~zDhKrc2BbxEk2NSHKQdF7WzcfB9rxpQv1vMxLydJtq2GE8oegF1E5KE0pCD2efZ9DilvgUlNZcoQjECdlRqoHTf1txf~k9RIqNPxjy3gYVOBvkgYIyA0ofRPAOe4a49fly1~frXT84MX3r~18UUQYA~1vpK2UFRj-4iAilOQoNoSzI4mMoqTA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-              alt="icono"
-            />
+            <PokeImage src={follower?.avatar_url} alt="icono" />
             {follower.login}
           </FollowersCard>
-        </FollowersContainer>
-      ))}
-      <Link to="/users/:username">Go back </Link>
+        ))}
+      </FollowersContainer>
+      <Link to="/">Go back </Link>
     </Wrapper>
   );
 }
--- src/pages/following-page.js
+++ src/pages/following-page.js
@@ -10,9 +10,7 @@ import Image1 from "../assets/Vector1.png";
 
 const MainTitle = styled.h1`
   display: flex;
-
   margin-bottom: 96px;
-
   font-size: 2em;
   margin-block-start: 0.67em;
   margin-block-end: 0.67em;
@@ -44,7 +42,6 @@ const FollowersCard = styled("div")`
   align-items: center;
   padding: 8px 12px;
   gap: 8px;
-
   width: 300px;
   height: 56px;
   ${typography.text.md};
@@ -66,7 +63,6 @@ const Pagination = styled.div`
   justify-content: center;
   padding: 4px 8px;
   gap: 8px;
-
   width: 202px;
   height: 30px;
   svg {
@@ -81,7 +77,6 @@ const PagButton = styled.button`
     current === children ? "#fff" : "#00000"};
   display: flex;
   flex-direction: column;
-
   padding: 1px 8px;
   gap: 10px;
 
@@ -131,17 +126,14 @@ function FollowingPage({ profile }) {
         <img src={Image1} alt="" onClick={handleIncresePage}></img>
       </Pagination>
 
-      {followings.map((following, index) => (
-        <FollowersContainer key={index}>
+      <FollowersContainer>
+        {followings.map((following) => (
           <FollowersCard value={following.login} key={following.id}>
-            <PokeImage
-              src="https://s3-alpha-sig.figma.com/img/5215/dacf/3b688b2a3dd2a02199de17ea0d7b5c09?Expires=1672012800&Signature=owRR5x8O9uqQQ1lQW3Ip4weQEhZfnrgUDiB6KoSAnCvLSxdDh8G6VbQzgKsj0IL9-tH8ADoo7AaUI2vOtbgtUo137po0DDuNzeai4ymqHMxuLEvhANS60nXzdg6tF6DBV6rs6kkuao28IT4pr3PVzqH-Lxz82mMb~zDhKrc2BbxEk2NSHKQdF7WzcfB9rxpQv1vMxLydJtq2GE8oegF1E5KE0pCD2efZ9DilvgUlNZcoQjECdlRqoHTf1txf~k9RIqNPxjy3gYVOBvkgYIyA0ofRPAOe4a49fly1~frXT84MX3r~18UUQYA~1vpK2UFRj-4iAilOQoNoSzI4mMoqTA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-              alt="icono"
-            />
+            <PokeImage src={following?.avatar_url} alt="icono" />
             {following.login}
           </FollowersCard>
-        </FollowersContainer>
-      ))}
+        ))}
+      </FollowersContainer>
       <Link to="/">Go back </Link>
     </Wrapper>
   );
